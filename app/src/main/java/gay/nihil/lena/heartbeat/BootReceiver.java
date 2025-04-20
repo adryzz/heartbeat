47512b46a98920d4882dc024471141aaf0cab396
Lena <lena@nihil.gay>
2025-04-20 23:32:59 +0200

    change example string

--- app/src/main/java/gay/nihil/lena/heartbeat/BootReceiver.java
+++ app/src/main/java/gay/nihil/lena/heartbeat/BootReceiver.java
@@ -8,8 +8,10 @@ import android.util.Log;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Log.i("BootReceiver", "Device booted. Starting service...");
+        if (Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.i("BootReceiver", "Device booted. Waiting for user to unlock device...");
+        } else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.i("BootReceiver", "Device unlocked. Starting service...");
             Intent serviceIntent = new Intent(context, AortaConnectionService.class);
             context.startForegroundService(serviceIntent);
         }
